259ccd3363a5c33dc94d9f5b81b6354141bc15f2
Christos Kokkalis <Uberck@gmail.com>
2025-10-15 20:25:53 -0400

    Code cleanup and review

--- PSWrapper.cpp
+++ PSWrapper.cpp
@@ -1,48 +1,34 @@
-// PSWrapper.cpp : Defines the entry point for the application.
-//
-
 #include "framework.h"
 #include "PSWrapper.h"
-#include <commctrl.h> // Include for ComboBox
+#include <commctrl.h>
 
 #define MAX_LOADSTRING 100
 
-// Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING] = L"Keolis Script Launcher v1.0"; // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING] = L"Keolis Script Launcher v1.0";
+WCHAR szWindowClass[MAX_LOADSTRING];
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
-
-    // Initialize global strings
     LoadStringW(hInstance, IDC_PSWRAPPER, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
+    if (!InitInstance(hInstance, nCmdShow))
         return FALSE;
-    }
 
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PSWRAPPER));
-
     MSG msg;
-
-    // Main message loop:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -51,140 +37,114 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
             DispatchMessage(&msg);
         }
     }
-
-    return (int) msg.wParam;
+    return (int)msg.wParam;
 }
 
-
-
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
-
     wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PSWRAPPER));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PSWRAPPER);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PSWRAPPER));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_PSWRAPPER);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
     return RegisterClassExW(&wcex);
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
-
-   // Window style: no resizing, no maximize/minimize
-   DWORD style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU;
-
-   // Set window size to 300x200 pixels
-   int winWidth = 240;
-   int winHeight = 200;
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, style,
-      CW_USEDEFAULT, 0, winWidth, winHeight, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
+    hInst = hInstance;
+    DWORD style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU;
+    int winWidth = 240, winHeight = 200;
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, style,
+        CW_USEDEFAULT, 0, winWidth, winHeight, nullptr, nullptr, hInstance, nullptr);
+    if (!hWnd)
+        return FALSE;
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+    return TRUE;
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE: Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    static HWND hComboBox = nullptr; // Store handle to ComboBox
-    static HWND hRunButton = nullptr; // Store handle to Button
-
+    static HWND hComboBox = nullptr;
+    static HWND hRunButton = nullptr;
     switch (message)
     {
     case WM_CREATE:
-        {
-            // Create a ComboBox control
-            hComboBox = CreateWindowEx(
-                0, WC_COMBOBOX, nullptr,
-                CBS_DROPDOWNLIST | WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_TABSTOP,
-                10, 10, 200, 100, // x, y, width, height
-                hWnd, (HMENU)1001, hInst, nullptr);
-
-            // Add items to the ComboBox
-            SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)L"Option 1");
-            SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)L"Option 2");
-            SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)L"Option 3");
-
-            // Create a "Run!" button right below the ComboBox
-            hRunButton = CreateWindowEx(
-                0, L"BUTTON", L"Run!",
-                WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                10, 70, 200, 30, // x, y, width, height (y = ComboBox y + height + margin)
-                hWnd, (HMENU)1002, hInst, nullptr);
-        }
+        hComboBox = CreateWindowEx(
+            0, WC_COMBOBOX, nullptr,
+            CBS_DROPDOWNLIST | WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_TABSTOP,
+            10, 10, 200, 100,
+            hWnd, (HMENU)1001, hInst, nullptr);
+        SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)L"Create User");
+        SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)L"Delete Profile");
+        SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)L"Find Employee ID");
+        hRunButton = CreateWindowEx(
+            0, L"BUTTON", L"Run!",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            10, 70, 200, 30,
+            hWnd, (HMENU)1002, hInst, nullptr);
         break;
     case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        switch (wmId)
         {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
+        case 1002:
+        {
+            int sel = (int)SendMessage(hComboBox, CB_GETCURSEL, 0, 0);
+            LPCWSTR scriptName = nullptr;
+            switch (sel)
             {
-            case 1002: // Run! button clicked
-                MessageBox(hWnd, L"You clicked Run!", L"Info", MB_OK | MB_ICONINFORMATION);
-                break;
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
+            case 0: scriptName = L"script1.ps1"; break;
+            case 1: scriptName = L"script2.ps1"; break;
+            case 2: scriptName = L"script3.ps1"; break;
             default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
+                MessageBox(hWnd, L"Please select a function from the dropdown.", L"Error", MB_OK | MB_ICONERROR);
+                return 0;
+            }
+            WCHAR cmdLine[512];
+            wsprintf(cmdLine, L"powershell.exe -ExecutionPolicy Bypass -File \"%s\"", scriptName);
+            STARTUPINFOW si = { sizeof(si) };
+            PROCESS_INFORMATION pi;
+            BOOL success = CreateProcessW(
+                nullptr, cmdLine, nullptr, nullptr, FALSE,
+                0, nullptr, nullptr, &si, &pi);
+            if (success) {
+                CloseHandle(pi.hProcess);
+                CloseHandle(pi.hThread);
+            }
+            else {
+                MessageBox(hWnd, L"Failed to launch PowerShell script.", L"Error", MB_OK | MB_ICONERROR);
             }
         }
         break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
         }
-        break;
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+    }
+    break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -194,7 +154,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-// Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
@@ -202,7 +161,6 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     {
     case WM_INITDIALOG:
         return (INT_PTR)TRUE;
-
     case WM_COMMAND:
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
         {
--- PSWrapper.h
+++ PSWrapper.h
@@ -1,3 +1,13 @@
 #pragma once
 
 #include "resource.h"
+#include <windows.h>
+
+extern HINSTANCE hInst;
+extern WCHAR szTitle[];
+extern WCHAR szWindowClass[];
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
